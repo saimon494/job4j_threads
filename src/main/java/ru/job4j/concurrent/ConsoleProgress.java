3fa5bd7a060a7de1126766ce8672086f6e8beadb
saimon494 <saimon.494@gmail.com>
2021-03-25 22:13:01 +0300

    3. Прерывание нити [#500393]

---
+++ src/main/java/ru/job4j/concurrent/ConsoleProgress.java
@@ -0,0 +1,26 @@
+package ru.job4j.concurrent;
+
+public class ConsoleProgress implements Runnable {
+
+    private final char[] process = {'\\', '|', '/', '-'};
+
+    @Override
+    public void run() {
+        int count = 0;
+        while (!Thread.currentThread().isInterrupted()) {
+            System.out.printf("\rload: %c", process[(count++) % 4]);
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                break;
+            }
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread progress = new Thread(new ConsoleProgress());
+        progress.start();
+        Thread.sleep(10000);
+        progress.interrupt();
+    }
+}
