613d08156f02db2d7ace3e39dcab13efa914948c
saimon494 <saimon.494@gmail.com>
2021-04-01 21:30:54 +0300

    0. Управление нитью через wait. [#500386]

---
+++ src/main/java/ru/job4j/wait/MultiCountBarrier.java
@@ -0,0 +1,32 @@
+package ru.job4j.wait;
+
+public class MultiCountBarrier {
+
+    public static void main(String[] args) {
+        CountBarrier countBarrier = new CountBarrier(30);
+
+        Thread counter = new Thread(() -> {
+            System.out.println(Thread.currentThread().getName());
+            for (int i = 0; i < 30; i++) {
+                countBarrier.count();
+            }
+        });
+        Thread thread1 = new Thread(
+                () -> {
+                    countBarrier.await();
+                    System.out.println(Thread.currentThread().getName());
+                },
+                "thread1 wait"
+        );
+        Thread thread2 = new Thread(
+                () -> {
+                    countBarrier.await();
+                    System.out.println(Thread.currentThread().getName());
+                },
+                "thread2 wait"
+        );
+        counter.start();
+        thread1.start();
+        thread2.start();
+    }
+}
