ecd7a8ed96bc16f305f9a73e55177a6cb2f1701b
saimon494 <saimon.494@gmail.com>
2021-04-04 19:45:14 +0300

    1. Visibility. Общий ресурс вне критической секции [#500403], исправление

--- src/main/java/ru/job4j/synch/ParseFile.java
+++ src/main/java/ru/job4j/synch/ParseFile.java
@@ -2,7 +2,6 @@ package ru.job4j.synch;
 
 import java.io.*;
 import java.util.function.Predicate;
-import java.util.stream.Collectors;
 
 public final class ParseFile {
 
@@ -12,26 +11,33 @@ public final class ParseFile {
         this.file = file;
     }
 
-    private String filterContent(String line, Predicate<Integer> filter) {
+    private String getContent(Predicate<Integer> pred) {
         StringBuffer buffer = new StringBuffer();
-        for (int index = 0; index < line.length(); index++) {
-            char c = line.charAt(index);
-            if (filter.test((int) c)) {
-                buffer.append(c);
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            int data;
+            while ((data = reader.read()) > 0) {
+                if (pred.test(data)) {
+                    buffer.append((char) data);
+                }
             }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
         return buffer.toString();
     }
 
-    private String getContent(Predicate<Integer> filter) throws IOException {
-        String content = "";
-        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
-            content = reader.lines()
-                    .map(str -> filterContent(str, filter))
-                    .collect(Collectors.joining(System.lineSeparator()));
-        }
-        return content;
-    }
+//    private String getContent(Predicate<Character> pred) throws IOException {
+//        String content = "";
+//        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+//            content = reader.lines()
+//                    .flatMapToInt(CharSequence::chars)
+//                    .mapToObj(c -> (char) c)
+//                    .filter(pred)
+//                    .map(String::valueOf)
+//                    .collect(Collectors.joining());
+//        }
+//        return content;
+//    }
 
     public synchronized String getContent() throws IOException {
         return getContent(c -> true);
@@ -39,5 +45,31 @@ public final class ParseFile {
 
     public synchronized String getContentWithoutUnicode() throws IOException {
         return getContent(c -> c < 0x80);
+    }
+
+    public static void main(String[] args) {
+        ParseFile parseFile = new ParseFile(new File("readme.md"));
+        String content = "";
+        SaveFile saveFile = new SaveFile(content, new File("readme2.md"));
+        Thread thread0 = new Thread(
+                () -> {
+                    try {
+                        saveFile.saveContent(parseFile.getContent());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println(Thread.currentThread().getName());
+                });
+        Thread thread1 = new Thread(
+                () -> {
+                    try {
+                        saveFile.saveContent(parseFile.getContentWithoutUnicode());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println(Thread.currentThread().getName());
+                });
+        thread0.start();
+        thread1.start();
     }
 }
